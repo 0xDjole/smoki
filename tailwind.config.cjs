a6bb480143f968c9bea405b9ececc7e0f54ce25e
Djole <0xdjole@gmail.com>
2024-02-15 20:31:48 +0100

    loader accent

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,62 +1,65 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {
-      backgroundColor: {
-        primary: "var(--primary-background-color)",
-        secondary: "var(--secondary-background-color)",
-        accent: "var(--accent-background-color)",
-        success: "var(--success-background-color)",
-        error: "var(--error-background-color)",
-      },
-      textColor: {
-        primary: "var(--primary-text-color)",
-        secondary: "var(--secondary-text-color)",
-        success: "var(--success-background-color)",
-        error: "var(--error-background-color)",
-      },
-    },
-  },
-  plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        ".border-primary": {
-          borderColor: "var(--primary-border-color)",
-          borderWidth: "1px",
-          borderStyle: "solid",
-        },
-        ".border-secondary": {
-          borderColor: "var(--primary-border-color)",
-          borderWidth: "1px",
-          borderStyle: "solid",
-        },
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				accent: 'var(--accent-color)'
+			},
+			backgroundColor: {
+				primary: 'var(--primary-background-color)',
+				secondary: 'var(--secondary-background-color)',
+				accent: 'var(--accent-background-color)',
+				success: 'var(--success-background-color)',
+				error: 'var(--error-background-color)'
+			},
+			textColor: {
+				primary: 'var(--primary-text-color)',
+				secondary: 'var(--secondary-text-color)',
+				success: 'var(--success-background-color)',
+				error: 'var(--error-background-color)'
+			}
+		}
+	},
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				'.border-primary': {
+					borderColor: 'var(--primary-border-color)',
+					borderWidth: '1px',
+					borderStyle: 'solid'
+				},
+				'.border-secondary': {
+					borderColor: 'var(--primary-border-color)',
+					borderWidth: '1px',
+					borderStyle: 'solid'
+				},
 
-        ".border-accent": {
-          borderColor: "var(--accent-border-color)",
-          borderWidth: "1px",
-          borderStyle: "solid",
-        },
+				'.border-accent': {
+					borderColor: 'var(--accent-border-color)',
+					borderWidth: '1px',
+					borderStyle: 'solid'
+				},
 
-        ".border-success": {
-          borderColor: "var(--success-border-color)",
-          borderWidth: "1px",
-          borderStyle: "solid",
-        },
+				'.border-success': {
+					borderColor: 'var(--success-border-color)',
+					borderWidth: '1px',
+					borderStyle: 'solid'
+				},
 
-        ".border-error": {
-          borderColor: "var(--error-border-color)",
-          borderWidth: "1px",
-          borderStyle: "solid",
-        },
+				'.border-error': {
+					borderColor: 'var(--error-border-color)',
+					borderWidth: '1px',
+					borderStyle: 'solid'
+				},
 
-        ".border-underline": {
-          borderBottomColor: "var(--primary-border-color)",
-          borderBottomWidth: "1px",
-          borderBottomStyle: "solid",
-        },
-      };
+				'.border-underline': {
+					borderBottomColor: 'var(--primary-border-color)',
+					borderBottomWidth: '1px',
+					borderBottomStyle: 'solid'
+				}
+			};
 
-      addUtilities(newUtilities, ["responsive", "hover"]);
-    },
-  ],
+			addUtilities(newUtilities, ['responsive', 'hover']);
+		}
+	]
 };

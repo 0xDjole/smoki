d704cd17dcd6b5207e8fa60f5aa771f7cb3fe691
Djole <0xdjole@gmail.com>
2024-02-21 00:32:46 +0100

    draw

--- src/lib/components/Drawer/index.svelte
+++ src/lib/components/Drawer/index.svelte
@@ -1,95 +1,96 @@
 <script>
-  import { onMount, onDestroy } from "svelte";
-  export let open = false;
-  export let duration = 0.8;
-  export let placement = "right";
-  export let size = null;
-  export let zIndex = -1;
-  export let height = null;
-  export let width = null;
-  export let left = null;
-  export let targetElement = null;
+	import { onMount, onDestroy } from 'svelte';
+	export let open = false;
+	export let duration = 0.8;
+	export let placement = 'right';
+	export let size = null;
+	export let zIndex = -1;
+	export let height = null;
+	export let width = null;
+	export let left = null;
+	export let targetElement = null;
 
-  let w = "100%";
+	let w = '100%';
 
-  let l = "0";
+	let l = '0';
 
-  let drawerTargetTop = 0;
+	let h = '100%';
 
-  $: h = `calc(100vh - ${drawerTargetTop}px)`;
-  let percentageX = 0;
+	let drawerTargetTop = 0;
 
-  $: if (left) {
-    l = left;
-  }
+	let percentageX = 0;
 
-  $: if (height) {
-    h = height;
-  }
+	$: if (left) {
+		l = left;
+	}
 
-  $: if (width) {
-    w = width;
-  }
+	$: if (height) {
+		h = height;
+	}
 
-  $: transform =
-    placement === "right"
-      ? `transform: translate(${percentageX}%, 0)`
-      : `transform: translate(0, ${percentageX}%)`;
+	$: if (width) {
+		w = width;
+	}
 
-  $: style = `--duration: ${duration}s; --size: ${size}; z-index: ${zIndex}; ${
-    percentageX > 0 && transform
-  }; height: ${h}; width: ${w}; left: ${l}px; top: ${drawerTargetTop}px`;
+	$: transform =
+		placement === 'right'
+			? `transform: translate(${percentageX}%, 0)`
+			: `transform: translate(0, ${percentageX}%)`;
 
-  $: if (open) {
-    percentageX = 0;
-  } else {
-    percentageX = 100;
-  }
+	$: style = `--duration: ${duration}s; --size: ${size}; z-index: ${zIndex}; ${
+		percentageX > 0 && transform
+	}; height: ${h}; width: ${w}; left: ${l}px; top: ${drawerTargetTop}px`;
 
-  $: drawerTargetTop = targetElement?.getBoundingClientRect()?.top;
+	$: if (open) {
+		percentageX = 0;
+	} else {
+		percentageX = 100;
+	}
 
-  function updatePosition() {
-    if (typeof window !== "undefined" && targetElement) {
-      drawerTargetTop = targetElement.getBoundingClientRect().top;
-    }
-  }
+	$: drawerTargetTop = targetElement?.getBoundingClientRect()?.top;
 
-  onMount(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", updatePosition);
-      updatePosition(); // initial calculation
-    }
-  });
+	function updatePosition() {
+		if (typeof window !== 'undefined' && targetElement) {
+			drawerTargetTop = targetElement.getBoundingClientRect().top;
+		}
+	}
 
-  onDestroy(() => {
-    if (typeof window !== "undefined") {
-      window.removeEventListener("resize", updatePosition);
-    }
-  });
+	onMount(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('resize', updatePosition);
+			updatePosition(); // initial calculation
+		}
+	});
+
+	onDestroy(() => {
+		if (typeof window !== 'undefined') {
+			window.removeEventListener('resize', updatePosition);
+		}
+	});
 </script>
 
 <div class="drawer" {style}>
-  <div class="panel {placement}">
-    <slot />
-  </div>
+	<div class="panel {placement}">
+		<slot />
+	</div>
 </div>
 
 <style type="text/postcss">
-  .drawer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    z-index: -1;
-    transition: transform var(--duration) ease;
-  }
+	.drawer {
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+		z-index: -1;
+		transition: transform var(--duration) ease;
+	}
 
-  .panel {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    z-index: 3;
-    overflow: hidden;
-  }
+	.panel {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: 3;
+		overflow: hidden;
+	}
 </style>

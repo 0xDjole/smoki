6eaf0c046644937dc3e55621d66c02e8de11327d
Djole <0xdjole@gmail.com>
2024-02-23 17:46:43 +0100

    modal fix

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -33,7 +33,7 @@
 	let loadedMoreBottom = false;
 
 	$: if (!currentItems.length && component) {
-		const height = parentHeight + 10 - listComponent.scrollHeight;
+		const height = component.clientHeight + 10 - listComponent.scrollHeight;
 		if (height > 0) {
 			spacer.style.height = `${height}px`;
 			spacerHeight = height;
@@ -44,7 +44,8 @@
 		if (component) {
 			if (currentItems.length !== items.length) {
 				tick().then(() => {
-					const height = parentHeight + 10 - listComponent.scrollHeight;
+					const height = component.clientHeight + 10 - listComponent.scrollHeight;
+					console.log('height', height);
 					if (height > 0) {
 						spacer.style.height = `${height}px`;
 						spacerHeight = height;

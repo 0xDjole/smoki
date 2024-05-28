43f0dd537db68e33f6bae58bc3bed113ba88519d
Djole <0xdjole@gmail.com>
2024-05-28 20:15:48 +0200

    dynamic

--- src/lib/components/DynamicFieldForm/index.svelte
+++ src/lib/components/DynamicFieldForm/index.svelte
@@ -4,7 +4,6 @@
 	import Range from '../Range/index.svelte';
 	import DropDown from '../DropDown/index.svelte';
 	import Select from '../NiceSelect/index.svelte';
-	import Thumbnail from '../Thumbnail/index.svelte';
 	import translateLabel from '../../utils/helpers/translateLabel';
 
 	import { locale } from 'svelte-i18n';
@@ -25,83 +24,86 @@
 		return label;
 	};
 	$: console.log('field congs ', fieldConfigs);
+	$: console.log('field', fields);
 </script>
 
 <div class="custom-field-config-body">
-	{#each fieldConfigs as fieldConfig, index}
+	{#each fieldConfigs as fieldConfig, index (fieldConfig.id)}
 		<div class="flex gap-x-2">
 			<div class="md:p-1 md:px-3 w-full box-border">
-				{#if fieldConfig?.properties?.options?.length}
-					{#if fieldConfig?.ui === 'nice_select'}
-						<Select
-							ui={fieldConfig?.ui}
-							isMultiSelect={fieldConfig?.properties.isMultiSelect}
-							position="horizontal"
+				{#if fields[index]}
+					{#if fieldConfig?.properties?.options?.length}
+						{#if fieldConfig?.ui === 'nice_select'}
+							<Select
+								ui={fieldConfig?.ui}
+								isMultiSelect={fieldConfig?.properties.isMultiSelect}
+								position="horizontal"
+								label={translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
+								labelThumbnail={fieldConfig.thumbnail}
+								bind:value={fields[index].value}
+								bind:errors={fields[index].errors}
+								options={fieldConfig.properties.options.map((option) => ({
+									label: parseLabel(option),
+									value: option
+								}))}
+							/>
+						{:else}
+							<DropDown
+								label={fieldConfig.key}
+								isMultiSelect={fieldConfig?.properties.isMultiSelect}
+								bind:value={fields[index].value}
+								bind:errors={fields[index].errors}
+								options={fieldConfig.properties.options.map((option) => ({
+									label: parseLabel(option),
+									value: option
+								}))}
+							/>
+						{/if}
+					{/if}
+
+					{#if fieldConfig?.properties?.isCustomInputAllowed}
+						<Input
+							label={!fieldConfig?.properties?.options?.length &&
+								translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
+							labelThumbnail={fieldConfig.thumbnail}
+							bind:value={fields[index].value}
+							bind:errors={fields[index].errors}
+							type={fieldConfig.type}
+							kind="primary"
+							placeholder="please_enter"
+						/>
+					{/if}
+
+					{#if fieldConfig?.properties?.range}
+						<div class={`${fieldConfig?.properties?.isCustomInputAllowed ? 'mt-3' : ''}`}>
+							<Range
+								label={fieldConfig?.properties?.isCustomInputAllowed
+									? null
+									: translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
+								bind:value={fields[index].value}
+								max={+fieldConfig.properties.range.max}
+								min={+fieldConfig.properties.range.min}
+								id="basic-slider"
+							/>
+						</div>
+					{/if}
+
+					{#if fieldConfig.type === 'boolean'}
+						<Switch
 							label={translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
 							labelThumbnail={fieldConfig.thumbnail}
 							bind:value={fields[index].value}
 							bind:errors={fields[index].errors}
-							options={fieldConfig.properties.options.map((option) => ({
-								label: parseLabel(option),
-								value: option
-							}))}
-						/>
-					{:else}
-						<DropDown
-							label={fieldConfig.key}
-							isMultiSelect={fieldConfig?.properties.isMultiSelect}
-							bind:value={fields[index].value}
-							bind:errors={fields[index].errors}
-							options={fieldConfig.properties.options.map((option) => ({
-								label: parseLabel(option),
-								value: option
-							}))}
 						/>
 					{/if}
-				{/if}
 
-				{#if fieldConfig?.properties?.isCustomInputAllowed}
-					<Input
-						label={!fieldConfig?.properties?.options?.length &&
-							translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
-						labelThumbnail={fieldConfig.thumbnail}
-						bind:value={fields[index].value}
-						bind:errors={fields[index].errors}
-						type={fieldConfig.type}
-						kind="primary"
-						placeholder="please_enter"
-					/>
-				{/if}
+					{#if fieldConfig.type === 'entities'}
+						<slot name="entities" idx={index} value={fields[index].value} {fieldConfig} />
+					{/if}
 
-				{#if fieldConfig?.properties?.range}
-					<div class={`${fieldConfig?.properties?.isCustomInputAllowed ? 'mt-3' : ''}`}>
-						<Range
-							label={fieldConfig?.properties?.isCustomInputAllowed
-								? null
-								: translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
-							bind:value={fields[index].value}
-							max={+fieldConfig.properties.range.max}
-							min={+fieldConfig.properties.range.min}
-							id="basic-slider"
-						/>
-					</div>
-				{/if}
-
-				{#if fieldConfig.type === 'boolean'}
-					<Switch
-						label={translateLabel(fieldConfig.label, $locale, fieldConfig.key)}
-						labelThumbnail={fieldConfig.thumbnail}
-						bind:value={fields[index].value}
-						bind:errors={fields[index].errors}
-					/>
-				{/if}
-
-				{#if fieldConfig.type === 'entities'}
-					<slot name="entities" idx={index} value={fields[index].value} {fieldConfig} />
-				{/if}
-
-				{#if fieldConfig.type === 'custom'}
-					<slot name="custom" idx={index} value={fields[index].value} {fieldConfig} />
+					{#if fieldConfig.type === 'custom'}
+						<slot name="custom" idx={index} value={fields[index].value} {fieldConfig} />
+					{/if}
 				{/if}
 			</div>
 		</div>

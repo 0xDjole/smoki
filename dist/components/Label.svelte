f44b755a893fabbce2aef2a9b5ab4a135105c1fd
Djole <0xdjole@gmail.com>
2024-05-30 03:41:20 +0200

    label

--- dist/components/Label.svelte
+++ dist/components/Label.svelte
@@ -18,43 +18,49 @@ export let errors = [];
 
 <style>
 	.thumbnail-wrapper {
-
-    display: flex;
-
-    align-items: flex-end;
-
-    -moz-column-gap: 0.5rem;
-
-         column-gap: 0.5rem
+		display: flex;
+		align-items: flex-end;
+		-moz-column-gap: 0.5rem;
+		     column-gap: 0.5rem;
 }
 
 	.label {
-
-    font-weight: 700;
-
-    --tw-text-opacity: 1;
-
-    color: rgb(255 255 255 / var(--tw-text-opacity))
+		font-weight: 700;
+		--tw-text-opacity: 1;
+		color: rgb(255 255 255 / var(--tw-text-opacity));
 }
 
 	@media (min-width: 768px) {
 
-    .label {
-
-        font-size: 1.25rem;
-
-        line-height: 1.75rem
-    }
+		.label {
+				font-size: 1.25rem;
+				line-height: 1.75rem;
+		}
 }
 
 	.thumbnail-container {
-
-    display: flex;
-
-    align-items: flex-end
+		display: flex;
+		align-items: flex-end;
 }
 
-	.label-error {
+	@keyframes shake {
+		0%,
+		100% {
+			transform: translateX(0);
+		}
+		25%,
+		75% {
+			transform: translateX(-5px);
+		}
+		50% {
+			transform: translateX(5px);
+		}
+	}
 
-    color: var(--error-background-color)
+	.label-error {
+		color: var(--error-background-color);
+		animation: shake 0.2s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+		transform: translate3d(0, 0, 0);
+		backface-visibility: hidden;
+		perspective: 1000px;
 }</style>

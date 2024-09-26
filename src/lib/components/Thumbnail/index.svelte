593c503f4881a9cb32d67f996e50d04a590977ab
Djole <0xdjole@gmail.com>
2024-09-26 19:11:06 +0200

    thumbnail

--- src/lib/components/Thumbnail/index.svelte
+++ src/lib/components/Thumbnail/index.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import { STORAGE_URL } from '../../utils/env';
 	import SvgIcon from '../SvgIcon.svelte';
-
 	import NoImage from '../../utils/icons/no-image.svg?raw';
-
-	import { onMount } from 'svelte';
 
 	export let url = null;
 	export let isPrefix = true;
@@ -12,22 +9,22 @@
 	export let size = 'md';
 
 	$: src = isPrefix ? `${STORAGE_URL}/${url}` : url;
-
 	let imageValid = true;
 
 	function handleImageError() {
 		imageValid = false;
 	}
 
-	onMount(() => {
+	$: if (src) {
+		imageValid = true; // Reset the validation state
 		const img = new Image();
 		img.src = src;
 		img.onload = () => (imageValid = true);
 		img.onerror = handleImageError;
-	});
+	}
 </script>
 
-<div class={`thumbnail`} class:sm={size === 'sm'} class:md={size === 'md'}>
+<div class="thumbnail" class:sm={size === 'sm'} class:md={size === 'md'}>
 	{#if imageValid}
 		<img class="image" {alt} {src} on:error={handleImageError} />
 	{:else}
@@ -39,15 +36,12 @@
 	.thumbnail {
 		@apply rounded-full overflow-hidden shadow-md;
 	}
-
 	.md {
 		@apply w-12 h-12 sm:w-16 sm:h-16;
 	}
-
 	.sm {
 		@apply w-8 h-8 sm:w-9 sm:h-9;
 	}
-
 	.image {
 		@apply h-full w-full;
 	}

020ca498f6b27192bfbbff62432556d9941aa9ef
Djole <0xdjole@gmail.com>
2024-10-05 11:06:16 +0200

    load

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -3,32 +3,23 @@
 </script>
 
 {#if show}
-	<div class="loader-container">
-		<div class="lds-dual-ring" />
-	</div>
+	<div class="lds-dual-ring" />
 {/if}
 
-<style type="text/postcss">
-	.loader-container {
-		@apply grid justify-self-center justify-center content-center;
+<style>
+	.lds-dual-ring {
+		display: block;
 		width: 35px;
 		height: 35px;
+		margin-left: auto;
+		margin-right: auto;
+		border: 6px solid var(--accent-color);
+		border-radius: 50%;
+		border-color: var(--accent-color) transparent;
+		animation: lds-dual-ring 1.2s linear infinite;
+		will-change: transform;
 	}
 
-	.lds-dual-ring {
-		display: inline-block;
-	}
-	.lds-dual-ring:after {
-		@apply border-[6px] border-solid;
-		content: ' ';
-		display: block;
-		width: 34px;
-		height: 34px;
-		margin: 8px;
-		border-radius: 50%;
-		border-color: var(--accent-color) transparent var(--accent-color) transparent;
-		animation: lds-dual-ring 1.2s linear infinite;
-	}
 	@keyframes lds-dual-ring {
 		0% {
 			transform: rotate(0deg);
